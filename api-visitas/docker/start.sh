e1f70bb8751bb78fb43ab9479ea7fd2e3c0bb163
MINC\rafael.gomez <rafaelogomezr@gmail.com>
2026-05-14 14:20:53 -0400

    Ajustando el proyecto para produccion

--- api-visitas/docker/start.sh
+++ api-visitas/docker/start.sh
@@ -16,6 +16,7 @@ if [ -f .env ]; then
   php artisan config:clear || true
   php artisan route:clear || true
   php artisan view:clear || true
+  php artisan storage:link || true
   php artisan config:cache || true
   php artisan route:cache || true
   php artisan view:cache || true
